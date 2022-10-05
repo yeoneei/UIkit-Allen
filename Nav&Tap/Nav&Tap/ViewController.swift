5be0ac32eb5f8e898426e582799c7a5420ffdbb6
yeoneei <soso3786@gmail.com>
2022-10-05 23:06:40 +0900

    Storyboard로 navigation 만들기

---
+++ Nav&Tap/Nav&Tap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tap
+//
+//  Created by 조연희 on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
